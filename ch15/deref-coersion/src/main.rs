de9840f00973c35c56d49329827d27a01b2566fd
Ivan Kuchin (ikuchin) <ivan.kuchin@gmail.com>
2023-12-31 13:46:52 -0500

    deref coersion

--- ch15/deref-coersion/src/main.rs
+++ ch15/deref-coersion/src/main.rs
@@ -1,4 +1,4 @@
-use std::{str::FromStr, ops::Deref};
+use std::{str::FromStr, ops::Deref, fmt};
 
 #[derive(Debug)]
 struct MyBox<T>(T);
@@ -17,16 +17,38 @@ impl<T> Deref for MyBox<T> {
     }
 }
 
-fn custom_print(s: &Vec<String>) {
-    println!("{:?}", s);
+impl fmt::Display for MyBox<Vec<String>> {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(f, "{:?}", self.0)
+    }
+}
+
+trait OutlinePrint: fmt::Display {
+    fn outline_print(&self) {
+        let s = self.to_string();
+        let len = s.len();
+
+        println!("{}", "*".repeat(len + 4));
+        println!("*{}*", " ".repeat(len + 2));
+        println!("* {} *", s);
+        println!("*{}*", " ".repeat(len + 2));
+        println!("{}", "*".repeat(len + 4));
+    }
+}
+
+impl OutlinePrint for MyBox<Vec<String>> {}
+
+fn deref_coersion(s: &Vec<String>) -> String {
+    format!("{:?}", s)
 }
 
 fn main() {
     let x = vec![String::from_str("hello").unwrap(), String::from_str("world").unwrap()];
-    println!("&Vec<String>");
-    custom_print(&x);
+    println!("&Vec<String>: {:?}", x);
     
-    let _y = MyBox::new(x);
-    println!("&MyBox<Vec<String>>");
-    custom_print(&_y);
+    let _y = MyBox::new(x.clone());
+    println!("&MyBox<Vec<String>>: {:?}", _y);
+    println!("&MyBox<Vec<String>> deref coersion: {}", deref_coersion(&_y));
+
+    _y.outline_print();
 }
